8b3c2fb3de8fe174b46a171fc0e0e1e8647e16cb
993113161 <993113161@qq.com>
2017-03-11 22:40:04 +0800

    P37

---
+++ app/src/main/java/com/example/a1231/text/FirstActivity.java
@@ -0,0 +1,29 @@
+package com.example.a1231.text;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by a1231 on 17/3/11.o
+ */
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        setContentView(R.layout.first_layout);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1",Toast.LENGTH_SHORT).show();  //给用户一个短的提示
+            }
+        });
+    }
+}
